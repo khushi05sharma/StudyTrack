7b2f117db6d5442b30ca705e95f3efd8350c8372
Khushi Sharma <khushisharma.50031@gmail.com>
2026-09-12 18:41:54 +0530

    created a user controller

---
+++ src/controllers/userController.js
@@ -0,0 +1,15 @@
+const pool = require("./db");
+
+const getUsers = async (req, res) => {
+  try {
+    const [rows] = await pool.execute("SELECT id, name, email FROM users");
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json({
+      message: "Failed to fetch users",
+    });
+  }
+};
+
