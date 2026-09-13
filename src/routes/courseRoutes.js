f75943d321959ddd4b2e08d763c5f683008ea773
Khushi Sharma <khushisharma.50031@gmail.com>
2026-09-13 19:38:11 +0530

    added routes for these courses

--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -9,3 +9,14 @@ const {
   deleteCourse,
 } = require("../controllers/courseController");
 
+router.get("/", getCourses);
+
+router.get("/:id", getCourseById);
+
+router.post("/", createCourse);
+
+router.put("/:id", updateCourse);
+
+router.delete("/:id", deleteCourse);
+
+module.exports = router;
