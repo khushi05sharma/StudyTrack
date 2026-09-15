3912209cde01a5c0291c3e3441d23bfc184cd0b6
Khushi Sharma <khushisharma.50031@gmail.com>
2026-09-15 19:23:01 +0530

    added get enrollment router in it

--- src/routes/enrollmentRoutes.js
+++ src/routes/enrollmentRoutes.js
@@ -1,2 +1,8 @@
 const express = require("express");
 const router = express.Router();
+
+const { getEnrollments } = require("../controllers/enrollmentController");
+
+router.get("/", getEnrollments);
+
+module.exports = router;
