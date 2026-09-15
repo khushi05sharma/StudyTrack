694e97535ba7ddaa08c93904f6929325c2af5319
Khushi Sharma <khushisharma.50031@gmail.com>
2026-09-15 19:19:15 +0530

    created enrollment, enrolled user and course in it

---
+++ src/controllers/enrollmentController.js
@@ -0,0 +1,24 @@
+const e = require("express");
+const pool = require("../db");
+
+const getEnrollments = async (req, res) => {
+  try {
+    const [rows] = await pool.execute(`
+            SELECT 
+            e.id AS enrollment_id,
+            u.name AS user_name,
+            c.title AS course_title,
+            e.enrolled_at            FROM enrollments e
+            JOIN users u ON e.user_id = u.id
+            JOIN courses c ON e.course_id = c.id
+            `);
+
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json({
+      message: "Failed to fetch enrollments",
+    });
+  }
+};
