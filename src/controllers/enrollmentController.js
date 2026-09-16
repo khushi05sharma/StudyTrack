0653a5f14b33cb647ebb751fe06e17fd3d7e7507
Khushi Sharma <khushisharma.50031@gmail.com>
2026-09-16 19:01:31 +0530

    added functionality to get enrollment through id

--- src/controllers/enrollmentController.js
+++ src/controllers/enrollmentController.js
@@ -23,6 +23,40 @@ const getEnrollments = async (req, res) => {
   }
 };
 
+// GET enrollment by ID
+
+const getEnrollmentById = async (req, res) => {
+  try {
+    const enrollmentId = req.params.id;
+
+    const [rows] = await pool.execute(
+      `
+      SELECT
+        e.id AS enrollment_id,
+        u.name AS user_name,
+        c.title AS course_title,
+        e.enrolled_at
+      FROM enrollments e
+      JOIN users u ON e.user_id = u.id
+      JOIN courses c ON e.course_id = c.id
+      WHERE e.id = ?
+    `,
+      [enrollmentId],
+    );
+
+    if (rows.length === 0) {
+      return res.status(404).json({ message: "enrollment not found" });
+    }
+    res.json(rows[0]);
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json({
+      message: "Failed to fetch enrollment",
+    });
+  }
+};
+
 module.exports = {
   getEnrollments,
 };
